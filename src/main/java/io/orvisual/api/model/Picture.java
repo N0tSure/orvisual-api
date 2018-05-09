bf14ad5e588e3cc6e7a0956af2c10b3a6476b562
N0tSure <ASir2089@gmail.com>
2018-05-10 01:58:36 +0300

    Created model of picture and JPA repository for it

---
+++ src/main/java/io/orvisual/api/model/Picture.java
@@ -0,0 +1,46 @@
+package io.orvisual.api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.Instant;
+
+/**
+ * Created on 09 May, 2018.
+ *
+ * Model of picture, used for maintain file storage.
+ * Attribute description:
+ * <li>
+ *     <ul>
+ *         {@code fileName} -- file's of file with extension
+ *     </ul>
+ *     <ul>
+ *         {@code directory} -- directory relative to gallery path
+ *     </ul>
+ *     <ul>
+ *         {@code loadedAt} -- Datetime of picture's uploading
+ *     </ul>
+ * </li>
+ *
+ * @author Artemis A. Sirosh
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Picture {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fileName;
+    private String directory;
+    private Instant loadedAt;
+
+}
---
+++ src/main/java/io/orvisual/api/repository/PictureRepository.java
@@ -0,0 +1,20 @@
+package io.orvisual.api.repository;
+
+import io.orvisual.api.model.Picture;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Optional;
+
+/**
+ * Created on 10 May, 2018.
+ *
+ * Provides persistent operations with {@link Picture}.
+ *
+ * @author Artemis A. Sirosh
+ */
+@RepositoryRestResource(exported = false)
+public interface PictureRepository extends CrudRepository<Picture, Long> {
+
+}
