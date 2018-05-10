f8db1a4781a3f966a5d4e5bf995c7ab881cb95de
N0tSure <ASir2089@gmail.com>
2018-05-10 03:50:48 +0300

    Created Spring Java config which will used to customize Rest Repositories

---
+++ src/main/java/io/orvisual/api/config/RepositoryConfig.java
@@ -0,0 +1,12 @@
+package io.orvisual.api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created on 09 May, 2018.
+ *
+ * @author Artemis A. Sirosh
+ */
+@Configuration
+public class RepositoryConfig {
+}
