58e6468c4d2ff480790542d918f2aa5d707a5018
N0tSure <ASir2089@gmail.com>
2018-05-20 19:51:26 +0300

    Dummy version of file controller and web configuration has been created

---
+++ src/main/java/io/orvisual/api/controller/FileController.java
@@ -0,0 +1,11 @@
+package io.orvisual.api.controller;
+
+/**
+ * <p>
+ * Created on 20.05.2018.
+ * </p>
+ *
+ * @author Artemis A. Sirosh
+ */
+public class FileController {
+}
---
+++ src/main/java/io/orvisual/api/controller/WebConfig.java
@@ -0,0 +1,14 @@
+package io.orvisual.api.controller;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * <p>
+ * Created on 20.05.2018.
+ * </p>
+ *
+ * @author Artemis A. Sirosh
+ */
+@Configuration
+public class WebConfig {
+}
