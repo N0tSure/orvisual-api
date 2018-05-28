5772d29c40c40d1451cf5d0a511a2d14810f4735
N0tSure <ASir2089@gmail.com>
2018-05-28 09:04:57 +0300

    Added argument matchers which able to ignore Picture timestamp field

--- src/test/java/io/orvisual/api/TestHelper.java
+++ src/test/java/io/orvisual/api/TestHelper.java
@@ -1,13 +1,20 @@
 package io.orvisual.api;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 import io.orvisual.api.model.Picture;
 import io.orvisual.api.model.PictureFileItem;
+import lombok.NonNull;
+import org.mockito.ArgumentMatcher;
 import org.springframework.http.MediaType;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.function.Supplier;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * <p>
@@ -19,6 +26,19 @@ import java.util.function.Supplier;
 public final class TestHelper {
 
     private static final byte[] OKLAHOMA_BYTES = new byte[]{'O', 'K', 'L', 'A', 'H', 'O', 'M', 'A'};
+
+    public static ArgumentMatcher<Picture> ignoreUnPredictableAttributes(@NonNull final Picture expectedPicture) {
+        return argument ->
+                expectedPicture.getChecksum().equals(argument.getChecksum()) &&
+                expectedPicture.getMimeType().equals(argument.getMimeType());
+    }
+
+    public static ArgumentMatcher<PictureFileItem> ignoreUnPredictableAttributes(@NonNull final PictureFileItem expectedItem) {
+        return argument ->
+                expectedItem.getPictureItem().getChecksum().equals(argument.getPictureItem().getChecksum()) &&
+                expectedItem.getPictureItem().getMimeType().equals(argument.getPictureItem().getMimeType()) &&
+                        Arrays.equals(expectedItem.getFileContent(), argument.getFileContent());
+    }
 
     public static Supplier<PictureFileItem> pictureSupplier() {
         final HashFunction sha256Function = Hashing.sha256();
