ec0f113c90932001341c8a4c55332c5e4db12284
N0tSure <ASir2089@gmail.com>
2018-05-28 09:25:14 +0300

    Test for Picture:FileController.saveImage(PictureFileItem) was completed

--- src/test/java/io/orvisual/api/controller/FileControllerTest.java
+++ src/test/java/io/orvisual/api/controller/FileControllerTest.java
@@ -5,37 +5,30 @@ import io.orvisual.api.model.PictureFileItem;
 import io.orvisual.api.repository.PictureRepository;
 import io.orvisual.api.service.FileStorageService;
 import io.orvisual.api.service.PictureFileProcessingException;
-import org.assertj.core.api.Assertions;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.BDDMockito;
 import org.mockito.Captor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 import java.util.function.Supplier;
 
+import static io.orvisual.api.TestHelper.ignoreUnPredictableAttributes;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -96,9 +89,10 @@ public class FileControllerTest {
 
     @Test
     public void shouldSaveNewPicture() throws Exception {
-        PictureFileItem expectedFileItem = fileItemSupplier.get();
-        when(pictureRepository.save(eq(expectedFileItem.getPictureItem())))
-                .thenReturn(expectedFileItem.getPictureItem());
+        final PictureFileItem expectedFileItem = fileItemSupplier.get();
+        when(pictureRepository.save(
+                argThat(ignoreUnPredictableAttributes(expectedFileItem.getPictureItem())))
+        ).thenReturn(expectedFileItem.getPictureItem());
 
         MockMultipartFile mockMultiPart = new MockMultipartFile(
                 "image",
@@ -114,8 +108,9 @@ public class FileControllerTest {
                 .andExpect(jsonPath("$.mimeType", equalTo(expectedFileItem.getPictureItem().getMimeType())));
 
         verify(pictureRepository).findById(eq(expectedFileItem.getPictureItem().getChecksum()));
-        verify(storageService).savePictureFileItem(eq(expectedFileItem));
-        verify(pictureRepository).save(expectedFileItem.getPictureItem());
+        verify(storageService).savePictureFileItem(argThat(ignoreUnPredictableAttributes(expectedFileItem)));
+        verify(pictureRepository)
+                .save(argThat(ignoreUnPredictableAttributes(expectedFileItem.getPictureItem())));
 
     }
 
@@ -125,8 +120,8 @@ public class FileControllerTest {
         doThrow(new PictureFileProcessingException("error while saving"))
                 .when(storageService).savePictureFileItem(any());
 
-        MockMultipartFile multipartFile =
-                new MockMultipartFile("image", "foo.jpg", MediaType.IMAGE_JPEG_VALUE,new byte[]{});
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "image", "foo.jpg", MediaType.IMAGE_JPEG_VALUE, new byte[]{2, 42});
 
         mockMvc.perform(multipart("/images").file(multipartFile))
                 .andDo(log())
@@ -136,4 +131,5 @@ public class FileControllerTest {
         verify(storageService).savePictureFileItem(any());
         verify(pictureRepository, never()).save(any());
     }
+
 }
