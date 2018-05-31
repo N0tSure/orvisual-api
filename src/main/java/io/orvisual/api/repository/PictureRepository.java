68993a202fcba92d592b627cc6f18d8f507cce97
N0tSure <ASir2089@gmail.com>
2018-05-31 20:37:03 +0300

    Exposed PictureRepository's safe and idempotent methods

--- src/main/java/io/orvisual/api/repository/PictureRepository.java
+++ src/main/java/io/orvisual/api/repository/PictureRepository.java
@@ -4,6 +4,7 @@ import io.orvisual.api.model.Picture;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
@@ -14,7 +15,16 @@ import java.util.Optional;
  *
  * @author Artemis A. Sirosh
  */
-@RepositoryRestResource(exported = false)
+@RepositoryRestResource
 public interface PictureRepository extends CrudRepository<Picture, String> {
 
+    @RestResource(exported = false)
+    @NonNull
+    @Override
+    <S extends Picture> S save(@NonNull S entity);
+
+    @RestResource(exported = false)
+    @NonNull
+    @Override
+    <S extends Picture> Iterable<S> saveAll(@NonNull Iterable<S> entities);
 }
