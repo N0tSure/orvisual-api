6007adc87a0762379d149883c4407da9001d37f8
N0tSure <ASir2089@gmail.com>
2018-05-31 23:30:48 +0300

    Tests for custom Data Rest created

--- src/test/java/io/orvisual/api/controller/PictureResourceTest.java
+++ src/test/java/io/orvisual/api/controller/PictureResourceTest.java
@@ -1,10 +1,20 @@
 package io.orvisual.api.controller;
 
+import io.orvisual.api.TestHelper;
+import io.orvisual.api.model.Picture;
+import io.orvisual.api.model.PictureFileItem;
+import io.orvisual.api.repository.PictureRepository;
 import io.orvisual.api.service.FileStorageService;
+import io.orvisual.api.service.PictureFileProcessingException;
+import org.hamcrest.Matchers;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,9 +28,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Supplier;
 
 import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -41,13 +57,22 @@ public class PictureResourceTest {
     private FileStorageService storageService;
 
     @Autowired
+    private PictureRepository pictureRepository;
+
+    @Autowired
     private WebApplicationContext webApplicationContext;
 
     private MockMvc mockMvc;
+    private final Supplier<PictureFileItem> itemSupplier = TestHelper.pictureSupplier();
 
     @Before
     public void setUpMockMvc() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @After
+    public void tearDownRepositories() {
+        pictureRepository.deleteAll();
     }
 
     @Test
@@ -63,6 +88,56 @@ public class PictureResourceTest {
                                 not("update-picture")
                         )
                 ));
+
+    }
+
+    @Test
+    public void shouldDeletePictureAndImageFile() throws Exception {
+        Picture fixturePicture = itemSupplier.get().getPictureItem();
+        fixturePicture = pictureRepository.save(fixturePicture);
+
+        assertNotNull("sanity check failed", pictureRepository.findById(fixturePicture.getChecksum()));
+
+        mockMvc.perform(delete("/pictures/" + fixturePicture.getChecksum()))
+                .andDo(log())
+                .andExpect(status().isNoContent());
+
+        assertFalse("fixture not removed", pictureRepository.existsById(fixturePicture.getChecksum()));
+        verify(storageService).deleteFile(fixturePicture);
+
+    }
+
+    @Test
+    public void shouldRejectDeletionNotExistedPicture() throws Exception {
+        Picture fixturePicture = itemSupplier.get().getPictureItem();
+
+        assertFalse("sanity check failed", pictureRepository.existsById(fixturePicture.getChecksum()));
+
+        mockMvc.perform(delete("/pictures/" + fixturePicture.getChecksum()))
+                .andDo(log())
+                .andExpect(status().isNotFound());
+
+        verify(storageService, never()).deleteFile(any());
+
+    }
+
+    @Test
+    public void shouldProcessErrorDuringFileDeletion() throws Exception {
+        Picture fixturePicture = itemSupplier.get().getPictureItem();
+        fixturePicture = pictureRepository.save(fixturePicture);
+
+        doThrow(new PictureFileProcessingException("deletion failed")).when(storageService).deleteFile(any());
+
+        assertTrue("sanity check failed", pictureRepository.existsById(fixturePicture.getChecksum()));
+
+        mockMvc.perform(delete("/pictures/" + fixturePicture.getChecksum()))
+                .andDo(log())
+                .andExpect(status().isInternalServerError());
+
+        assertFalse("fixture must be removed first",
+                pictureRepository.existsById(fixturePicture.getChecksum()));
+
+        verify(storageService).deleteFile(fixturePicture);
 
     }
 }
